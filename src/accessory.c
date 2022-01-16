83713c9a2d6b2af6a7d8e6a1a848847d498899b3
Rulee Chen <rulee@live.cn>
2022-01-16 13:38:10 +0800

    feat(homekit): setup homekit env

---
+++ src/accessory.c
@@ -0,0 +1,60 @@
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+
+void onAccessoryIdentify(homekit_value_t value) {
+  printf("accessory identify\n");
+}
+
+homekit_characteristic_t accessoryManufacturer = HOMEKIT_CHARACTERISTIC_(MANUFACTURER, VICTOR_ACCESSORY_INFORMATION_MANUFACTURER);
+homekit_characteristic_t accessorySerialNumber = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, VICTOR_ACCESSORY_INFORMATION_SERIAL_NUMBER);
+homekit_characteristic_t accessoryModel        = HOMEKIT_CHARACTERISTIC_(MODEL, VICTOR_ACCESSORY_INFORMATION_MODEL);
+homekit_characteristic_t accessoryVersion      = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION, VICTOR_FIRMWARE_VERSION);
+homekit_characteristic_t accessoryIdentify     = HOMEKIT_CHARACTERISTIC_(IDENTIFY, onAccessoryIdentify);
+homekit_characteristic_t accessoryName         = HOMEKIT_CHARACTERISTIC_(NAME, VICTOR_ACCESSORY_SERVICE_NAME); // change on setup
+
+homekit_service_t informationService = HOMEKIT_SERVICE_(
+  ACCESSORY_INFORMATION,
+  .primary = false,
+  .characteristics = (homekit_characteristic_t*[]) {
+    &accessoryManufacturer,
+    &accessorySerialNumber,
+    &accessoryModel,
+    &accessoryVersion,
+    &accessoryIdentify,
+    &accessoryName,
+    NULL,
+  },
+);
+
+// format: float; min 0, max 100, step 0.1, unit celsius
+homekit_characteristic_t temperatureState = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
+// format: float; min 0, max 100, step 1
+homekit_characteristic_t humidityState = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+
+homekit_service_t stateService = HOMEKIT_SERVICE_(
+  GARAGE_DOOR_OPENER,
+  .primary = true,
+  .characteristics = (homekit_characteristic_t*[]) {
+    &temperatureState,
+    &humidityState,
+    NULL,
+  },
+);
+
+homekit_accessory_t* accessories[] = {
+  HOMEKIT_ACCESSORY(
+    .id = 1,
+    .category = homekit_accessory_category_sensor,
+    .services = (homekit_service_t*[]) {
+      &informationService,
+      &stateService,
+      NULL,
+    },
+  ),
+  NULL,
+};
+
+homekit_server_config_t serverConfig = {
+  .accessories = accessories,
+  .password = VICTOR_ACCESSORY_SERVER_PASSWORD,
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,78 @@
 #include <Arduino.h>
+#include <arduino_homekit_server.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Console.h>
+#include <BuiltinLed.h>
+#include <VictorOTA.h>
+#include <VictorWifi.h>
+#include <VictorWeb.h>
+
+using namespace Victor;
+using namespace Victor::Components;
+
+extern "C" homekit_characteristic_t temperatureState;
+extern "C" homekit_characteristic_t humidityState;
+extern "C" homekit_characteristic_t accessoryName;
+extern "C" homekit_server_config_t serverConfig;
+
+VictorWeb webPortal(80);
+String hostName;
+
+String parsePercent(float state) {
+  return String(state) + "%";
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+String parseYesNo(bool state) {
+  return state == true ? F("Yes") : F("No");
+}
+
+void setup(void) {
+  console.begin(115200);
+  if (!LittleFS.begin()) {
+    console.error()
+      .bracket(F("fs"))
+      .section(F("mount failed"));
+  }
+
+  builtinLed.setup();
+  builtinLed.turnOn();
+
+  // setup web
+  webPortal.onRequestStart = []() { builtinLed.toggle(); };
+  webPortal.onRequestEnd = []() { builtinLed.toggle(); };
+  webPortal.onServiceGet = [](std::vector<KeyValueModel>& items) {
+    items.push_back({ .key = F("Service"),     .value = VICTOR_ACCESSORY_SERVICE_NAME });
+    items.push_back({ .key = F("Temperature"), .value = String(temperatureState.value.float_value) });
+    items.push_back({ .key = F("Humidity"),    .value = parsePercent(humidityState.value.float_value) });
+    items.push_back({ .key = F("Paired"),      .value = parseYesNo(homekit_is_paired()) });
+    items.push_back({ .key = F("Clients"),     .value = String(arduino_homekit_connected_clients_count()) });
+  };
+  webPortal.onServicePost = [](const String type) {
+    if (type == F("reset")) {
+      homekit_server_reset();
+    }
+  };
+  webPortal.setup();
+
+  // setup sensor
+  //
+
+  // setup homekit server
+  hostName = victorWifi.getHostName();
+  accessoryName.value.string_value = const_cast<char*>(hostName.c_str());
+  arduino_homekit_setup(&serverConfig);
+
+  // setup wifi
+  victorOTA.setup();
+  victorWifi.setup();
+
+  // done
+  console.log()
+    .bracket(F("setup"))
+    .section(F("complete"));
+}
+
+void loop(void) {
+  webPortal.loop();
+  arduino_homekit_loop();
 }
