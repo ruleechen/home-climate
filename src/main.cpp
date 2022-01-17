15300967674694d626764068a1b9909e7e2d4c0c
Rulee Chen <rulee@live.cn>
2022-01-17 21:42:46 +0800

    feat(sensor): setup aht10 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <Adafruit_AHTX0.h>
 #include <Adafruit_SGP30.h>
 #include <arduino_homekit_server.h>
 
@@ -15,11 +16,14 @@
 using namespace Victor;
 using namespace Victor::Components;
 
+extern "C" homekit_characteristic_t temperatureState;
+extern "C" homekit_characteristic_t humidityState;
 extern "C" homekit_characteristic_t airQualityState;
 extern "C" homekit_characteristic_t vocDensityState;
 extern "C" homekit_characteristic_t accessoryName;
 extern "C" homekit_server_config_t serverConfig;
 
+Adafruit_AHTX0 aht;
 Adafruit_SGP30 sgp;
 VictorWeb webPortal(80);
 String hostName;
@@ -44,8 +48,31 @@ String parseYesNo(bool state) {
   return state == true ? F("Yes") : F("No");
 }
 
+uint32_t getAbsoluteHumidity(float temperature, float humidity) {
+  // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
+  const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
+  const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
+  return absoluteHumidityScaled;
+}
+
 void measure(bool notify) {
   builtinLed.flash();
+  // aht
+  sensors_event_t humidity, temp;
+  if (aht.getEvent(&humidity, &temp)) {
+    temperatureState.value.float_value = temp.temperature;
+    humidityState.value.float_value = humidity.relative_humidity;
+    sgp.setHumidity(getAbsoluteHumidity(temp.temperature, humidity.relative_humidity));
+    if (notify) {
+      homekit_characteristic_notify(&temperatureState, temperatureState.value);
+      homekit_characteristic_notify(&humidityState, humidityState.value);
+    }
+    console.log()
+      .bracket(F("aht10"))
+      .section(F("temperature"), String(temp.temperature))
+      .section(F("humidity"), String(humidity.relative_humidity));
+  }
+  // sgp
   if (sgp.IAQmeasure()) {
     vocDensityState.value.float_value = sgp.TVOC;
     airQualityState.value.uint8_value = parseAirQuality(sgp.TVOC);
@@ -58,12 +85,12 @@ void measure(bool notify) {
       .section(F("TVOC"), String(sgp.TVOC))
       .section(F("eCO2"), String(sgp.eCO2));
   }
-  if (sgp.IAQmeasureRaw()) {
-    console.log()
-      .bracket(F("sgp30"))
-      .section(F("RawH2"), String(sgp.rawH2))
-      .section(F("RawEthanol"), String(sgp.rawEthanol));
-  }
+  // if (sgp.IAQmeasureRaw()) {
+  //   console.log()
+  //     .bracket(F("sgp30"))
+  //     .section(F("RawH2"), String(sgp.rawH2))
+  //     .section(F("RawEthanol"), String(sgp.rawEthanol));
+  // }
 }
 
 void setup(void) {
@@ -82,6 +109,8 @@ void setup(void) {
   webPortal.onRequestEnd = []() { builtinLed.toggle(); };
   webPortal.onServiceGet = [](std::vector<KeyValueModel>& items) {
     items.push_back({ .key = F("Service"),     .value = VICTOR_ACCESSORY_SERVICE_NAME });
+    items.push_back({ .key = F("Temperature"), .value = String(temperatureState.value.float_value) + F("C") });
+    items.push_back({ .key = F("Humidity"),    .value = String(humidityState.value.float_value) + F("%") });
     items.push_back({ .key = F("Air Quality"), .value = parseStateName(airQualityState.value.uint8_value) });
     items.push_back({ .key = F("VOC Density"), .value = String(vocDensityState.value.float_value) });
     items.push_back({ .key = F("Paired"),      .value = parseYesNo(homekit_is_paired()) });
@@ -106,14 +135,15 @@ void setup(void) {
     model.sdaPin, // Inter-Integrated Circuit - Serial Data (I2C-SDA)
     model.sclPin  // Inter-Integrated Circuit - Serial Clock (I2C-SCL)
   );
+  if (aht.begin()) {
+    console.log()
+      .bracket(F("aht10"))
+      .section(F("begin"));
+  }
   if (sgp.begin()) {
     console.log()
       .bracket(F("sgp30"))
-      .section(F("serial"), String(sgp.serialnumber[0], HEX) + String(sgp.serialnumber[1], HEX) + String(sgp.serialnumber[2], HEX));
-  } else {
-    console.error()
-      .bracket(F("sgp30"))
-      .section(F("init failed"));
+      .section(F("begin"));
   }
 
   // setup wifi
