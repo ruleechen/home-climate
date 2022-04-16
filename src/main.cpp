8ad117c17c5dcc0c5849ad370ad2b9876be8e9b4
rulee.chen <rulee@live.cn>
2022-04-16 23:13:20 +0800

    chore(infra): update to home-esp8266@0.4.3

--- src/main.cpp
+++ src/main.cpp
@@ -190,11 +190,12 @@ void setup(void) {
     states.push_back({ .text = F("Paired"),      .value = toYesNoName(homekit_is_paired()) });
     states.push_back({ .text = F("Clients"),     .value = String(arduino_homekit_connected_clients_count()) });
     // buttons
+    buttons.push_back({ .text = F("Unpair"), .value = F("Unpair") });
     buttons.push_back({ .text = F("aht10"), .value = F("aht10") });
     buttons.push_back({ .text = F("sgp30"), .value = F("sgp30") });
   };
   webPortal.onServicePost = [](const String& value) {
-    if (value == F("Reset")) {
+    if (value == F("Unpair")) {
       homekit_server_reset();
     } else if (value == F("aht10")) {
       aht10.softReset();
